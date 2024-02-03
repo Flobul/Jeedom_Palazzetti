495eef612f917d0bf15366cc5f789f7e9cf58e6b
Flobul <46993341+Flobul@users.noreply.github.com>
2024-02-03 13:56:20 +0100

    Add unit. Add static commentary from contructor. Better display on small devices.

--- desktop/modal/parametres.php
+++ desktop/modal/parametres.php
@@ -3,11 +3,34 @@
     if (!is_object($eqLogic)) {
         throw new Exception(__('Objet non trouvé', __FILE__));
     }
-    sendVarToJS('id', $_GET['id']);
-
+    sendVarToJS('eqPalaId', $_GET['id']);
     sendVarToJS('eqLogic', utils::o2a($eqLogic));
-
 ?>
+<style>
+  @media (max-width: 48em) {
+    #table_param .param td:nth-child(3),
+    #table_param > thead > tr > th:nth-child(3) {
+        display: none;
+    }
+    #table_param .hideMe {
+        display: none;
+    }
+  }
+  .paramAttr[data-l1key=id] {
+      width: 40px;
+  }
+  #table_param .param td:nth-child(6) {
+    display: inline-flex;
+  }
+  .paramAttr[data-l1key=unit] {
+      word-break: normal;
+      width: 65px;
+  }
+  .paramAttr[data-l1key=value] {
+      width: 100px;
+  }
+
+</style>
 <div role="tabpanel" class="tab-pane" id="paramtab">
     <span class="input-group pull-right">
         <a class="btn btn-success btn-sm paramAction roundedLeft" data-action="saveComments"><i class="fas fa-save"></i> {{Sauvegarder les commentaires}}</a>
@@ -23,6 +46,7 @@
                 <th>{{Description}}</th>
                 <th>{{Commentaire}}</th>
                 <th>{{Valeur}}</th>
+                <th>{{Unité}}</th>
                 <th>{{Action}}</th>
             </tr>
         </thead>
